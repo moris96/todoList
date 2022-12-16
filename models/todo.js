69eeb8eacbcb0de1a890c3ffb1fdb997e017be1b
moris96 <moriskhoudari@gmail.com>
2022-12-16 11:25:51 -0500

    todo lab done

---
+++ models/todo.js
@@ -0,0 +1,12 @@
+const {Schema, model} = require('mongoose')
+
+const todoSchema = new Schema({
+    title: String,
+    completed: Boolean 
+}, {
+    timestamps:true
+})
+
+const Todo = model('Todo', todoSchema)
+
+module.exports = Todo
---
+++ seed/seed.js
@@ -0,0 +1,15 @@
+// use this for seed data
+const toDoListData = [
+    {
+      title: "Learn more about React",
+      completed: true
+    },
+    {
+      title: "Write a new Component",
+      completed: false
+    },
+    {
+      title: "Add some style",
+      completed: false
+    }
+  ];
